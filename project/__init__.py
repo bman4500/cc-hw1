4732cd2955de2d0ba6f0f5df4925ff2e697d57bf
bman4500 <31734180+bman4500@users.noreply.github.com>
2020-02-19 17:23:14 -0500

    Add files via upload

--- project/__init__.py
+++ project/__init__.py
@@ -9,8 +9,6 @@ def create_app():
 
     app.config['SECRET_KEY'] = 'thisismysecretkeydonotstealit'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0')
 
     db.init_app(app)
 
